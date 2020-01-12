08a60b19855d1162d57b636acf06b3f1c42ea4d8
bpzhang <zbp2014@gmail.com>
2020-01-12 13:34:19 +0800

    init

---
+++ src/main/java/io/github/distributedtools/DistributedToolsApplication.java
@@ -0,0 +1,13 @@
+package io.github.distributedtools;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DistributedToolsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DistributedToolsApplication.class, args);
+	}
+
+}
